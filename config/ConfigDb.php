46d128079921d44dbf4ac67dfe19edfcbc5feaa2
root <root@dmitriy-K53SD>
2024-03-01 16:23:02 +0300

    Update

--- config/ConfigDb.php
+++ config/ConfigDb.php
@@ -4,7 +4,7 @@ class ConfigDb
     const DBDRIVER = "mysql";
     const DBHOST = "127.0.0.1";
     const DBPORT = "3306";
-    const DBNAME = "mvc";
-    const DBUSER = "dmitriy";
-    const DBPASS = "example";
+    const DBNAME = "you_db";
+    const DBUSER = "you_name";
+    const DBPASS = "you_password";
 }
